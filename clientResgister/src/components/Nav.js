71639c1e861deb7b6b579283d7558986b47075ee
Fabio Schitini <fabioschitini1@hotmail.com>
2022-06-17 21:27:32 -0300

    .

--- clientResgister/src/components/Nav.js
+++ clientResgister/src/components/Nav.js
@@ -1,19 +1,23 @@
 import {Link} from 'react-router-dom'
-import { Navbar,Nav,Container} from 'react-bootstrap';
+import { Navbar,Nav,Container,NavDropdown} from 'react-bootstrap';
 
 
 const Navs = (props) => {
     return ( 
 
-<Navbar style={{marginBottom:"20px"}} bg="dark" variant="dark">
-    <Container>
-    <Navbar.Brand href="#home">Clientes do meu app</Navbar.Brand>
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container>
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
     <Link to="/Registrar-Clientes">  <Nav.Link href="#home">Home</Nav.Link></Link>
     <Link to="/Registrar-Clientes/search"> <Nav.Link href="#features">Search</Nav.Link> </Link>
     </Nav>
-    </Container>
-  </Navbar>
+  
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
 
            
     )
